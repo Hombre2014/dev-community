3b60d314cca38e0cfd2b2c4a04693440647d291f
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-07-25 22:21:09 +0200

    Add fake users data

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  PROFILE_TITLE = [
+  'Senior Ruby on Rails Developer',
+  'Mid-level Ruby on Rails Developer',
+  'Full Stack Ruby on Rails Developer',
+  'Ruby on Rails Developer',
+  'Junior Ruby on Rails Developer',
+  'Senior React Developer',
+  ].freeze
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ActiveRecord::Base.transaction do
+  100.times do |i|
+    user = User.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      username: "#{Faker::Name.first_name.downcase}_#{i}",
+      email: Faker::Internet.email,
+      contact_number: Faker::PhoneNumber.phone_number_with_country_code,
+      street_address: Faker::Address.street_address,
+      city: Faker::Address.city,
+      state: Faker::Address.state,
+      country: Faker::Address.country,
+      postcode: Faker::Address.postcode,
+      date_of_birth: Faker::Date.birthday(min_age: 18, max_age: 65),
+      profile_title: User::PROFILE_TITLE.sample,
+      password: "password",
+      about: Faker::Lorem.paragraph(sentence_count: 4)
+    )
+
+    puts "User #{user.username} created!"
+  end
+
+end
