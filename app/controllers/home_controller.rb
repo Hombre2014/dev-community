c8f35d650ee60bb9dde2538b7711dd59657c13f6
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-07-26 16:42:58 +0200

    Fixing rubocop warnings

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
   def index
     @q = User.ransack(params[:q])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -5,27 +7,27 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :trackable
 
   PROFILE_TITLE = [
-  'Senior Ruby on Rails Developer',
-  'Mid-level Ruby on Rails Developer',
-  'Full Stack Ruby on Rails Developer',
-  'Ruby on Rails Developer',
-  'Junior Ruby on Rails Developer',
-  'Senior React Developer',
-  'Mid-level React Developer',
-  'Front End React Developer',
-  'Back End Ruby Engineer',
-  'Senior Ruby Engineer',
+    'Senior Ruby on Rails Developer',
+    'Mid-level Ruby on Rails Developer',
+    'Full Stack Ruby on Rails Developer',
+    'Ruby on Rails Developer',
+    'Junior Ruby on Rails Developer',
+    'Senior React Developer',
+    'Mid-level React Developer',
+    'Front End React Developer',
+    'Back End Ruby Engineer',
+    'Senior Ruby Engineer'
   ].freeze
 
   def name
     "#{first_name} #{last_name}".strip
   end
 
-  def self.ransackable_attributes(auth_object = nil)
-    ['country', 'city']
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[country city]
   end
 
-  def self.ransackable_associations(auth_object = nil)
+  def self.ransackable_associations(_auth_object = nil)
     []
   end
 end
