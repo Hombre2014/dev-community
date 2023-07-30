a5d37e9c97909b79d55778bfb82ad6b72dd74040
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-07-30 11:16:14 +0200

    Add work_experience model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
 
+  has_many :work_experiences
+
   PROFILE_TITLE = [
     'Senior Ruby on Rails Developer',
     'Mid-level Ruby on Rails Developer',
