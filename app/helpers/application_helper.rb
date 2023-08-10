4cc7d0084fff32146d978f7b792195c1b40a1d69
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-08-10 17:46:15 +0200

    Refactor the status indicator.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def get_connection_status(current_user, user)
+    current_user.connections.find_by(connected_user_id: user.id).status
+  end
 end
