b10f8cd3710309a1147b513dbe7ff495dbe35c35
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-11-28 21:21:26 +0100

    Add letter opener gem and modify confirmations and forgot passwords screens.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions', passwords: 'users/passwords', confirmations: 'users/confirmations' }
   root 'home#index'
 
   get 'member/:id', to: 'members#show', as: 'member'
